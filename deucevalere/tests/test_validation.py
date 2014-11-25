50532f76721bc1374880480d34eccf09a1a5ccbf
BenjamenMeyer <ben.meyer@rackspace.com>
2014-11-25 18:46:57 +0000

    Enhancement: Testing Expire Age Rules
    - Added tests for the Expiration Age validation rules

--- deucevalere/tests/test_validation.py
+++ deucevalere/tests/test_validation.py
@@ -1,6 +1,7 @@
 """
 Deuce Valere - Tests - Common - Validation
 """
+import datetime
 import unittest
 
 import deuceclient.client.deuce as client
@@ -79,3 +80,62 @@ class TestDeuceClientRule(TestRulesBase):
         for case in TestDeuceClientRule.negative_cases:
             with self.assertRaises(TypeError):
                 self.check_client(case)
+
+
+class TestExpireAgeRule(TestRulesBase):
+
+    positive_cases = [
+        datetime.timedelta(microseconds=1),
+        datetime.timedelta(milliseconds=1),
+        datetime.timedelta(seconds=2),
+        datetime.timedelta(minutes=3),
+        datetime.timedelta(hours=5),
+        datetime.timedelta(days=8),
+        datetime.timedelta(weeks=13)
+    ]
+
+    negative_cases = [
+        None,
+        1, 1, 2, 3, 5, 8, 13,
+        'Monday', b'Tuesday', u'Wednesday',
+        datetime.datetime.max,
+        datetime.datetime.utcnow(),
+        datetime.date.max,
+        datetime.date.today(),
+        datetime.time.max,
+        datetime.datetime.utcnow().time()
+    ]
+
+    @validate(expires=v.ExpireAgeRule)
+    def check_expires(self, expires):
+        return True
+
+    @validate(expires=v.ExpireAgeRuleNoneOkay)
+    def check_expires_with_none(self, expires):
+        return True
+
+    def test_expire_age_instance(self):
+        for p_case in TestExpireAgeRule.positive_cases:
+            v.val_expire_age()(p_case)
+
+        for case in TestExpireAgeRule.negative_cases:
+            with self.assertRaises(v.ValidationFailed):
+                v.val_expire_age()(case)
+
+    def test_expire_age_rule(self):
+        for p_case in TestExpireAgeRule.positive_cases:
+            self.check_expires(p_case)
+
+        for case in TestExpireAgeRule.negative_cases:
+            with self.assertRaises(TypeError):
+                self.check_expires(case)
+
+    def test_expire_age_rule_with_none(self):
+        positive_cases, negative_cases = self.cases_with_none_okay()
+
+        for p_case in positive_cases:
+            self.check_expires_with_none(p_case)
+
+        for case in negative_cases:
+            with self.assertRaises(TypeError):
+                self.check_expires_with_none(case)
